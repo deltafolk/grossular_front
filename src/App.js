07e4af88ec84a17a535fd40f088fb47d08362563
GISNAN <deltafolk144@gmail.com>
2022-10-21 15:32:03 +0700

    first react test

--- src/App.js
+++ src/App.js
@@ -1,23 +1,58 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+let mainURL = 'http://localhost:7000';
+let loginURL = 'http://localhost:7000/loginresult';
 
 function App() {
+
+  let [post, setPost] = useState(null);
+  const [username, setUserName] = useState();
+  const [password, setPassword] = useState();
+
+
+  useEffect(()=>{
+    axios.get(mainURL).then((res) => {
+      setPost(res.data);
+      console.log(res.data);
+    })
+  }, []);
+
+  function getPost(){
+    axios.post(loginURL, {
+      username,
+      password
+    }
+    ).then((res) => {
+      setPost(res.data)
+      console.log(res.data);
+    })
+  }
+
+  if (!post) return "null";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>TITLE : {post.title}{post.off}</h1>
+      
+      <Form>
+        <Form.Group className="mb-3 px-5">
+        <p>Username</p>
+          <Form.Control type="email" placeholder="Enter Username" onChange={e => setUserName(e.target.value)}/>
+        </Form.Group>
+
+        <Form.Group className="mb-3 px-5" controlId="formBasicPassword">
+          <p>Password</p>
+          <Form.Control type="password" placeholder="Enter Password" onChange={e => setPassword(e.target.value)}/>
+        </Form.Group>
+
+        <Button onClick={getPost} variant='primary'>
+          Submit
+        </Button>
+      </Form>
     </div>
   );
 }
